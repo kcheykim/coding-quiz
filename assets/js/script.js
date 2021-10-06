426c0279930ee2a7ee316ac55babb1bb40396e18
kcheykim <kosalcheykim@gmail.com>
2021-10-05 23:13:36 -0700

    creating questions for the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,103 @@
+//creating an array of 10 objects with question and list of answers to choose from
+const quizQuestion = [
+    {
+      question: "What handles the structure of the webpage?",
+      answers: {
+        a: "CSS",
+        b: "javaScript",
+        c: "HTML",
+        d: "git"
+      },
+      correctAns: "c"
+    },
+    {
+      question: "What handles the way webpages look?",
+      answers: {
+        a: "CSS",
+        b: "javaScript",
+        c: "HTML",
+        d: "git"
+      },
+      correctAns: "a"
+    },
+    {
+      question: "What makes the webpages interactive?",
+      answers: {
+        a: "CSS",
+        b: "javaScript",
+        c: "HTML",
+        d: "git"
+      },
+      correctAns: "b"
+    },
+    {
+      question: "What is the command before you commit a page?",
+      answers: {
+        a: "git push origin main",
+        b: "git add -A",
+        c: "git checkout -b main",
+        d: "git commit -m 'message'"
+      },
+      correctAns: "b"
+    },
+    {
+      question: "What are the parts that lies inside the <body> tag in html?",
+      answers: {
+        a: "head, body, foot",
+        b: "header, body, footer",
+        c: "html, body, foot",
+        d: "header, content, footer"
+      },
+      correctAns: "d"
+    },
+    {
+      question: "In javaScript,  --> var classroom = []; is a declaration of a(n)?",
+      answers: {
+        a: "function",
+        b: "array",
+        c: "variable",
+        d: "class'"
+      },
+      correctAns: "b"
+    },
+    {
+      question: "What has the highest priority in CSS?",
+      answers: {
+        a: "an id",
+        b: "a class",
+        c: "a flexbox",
+        d: "a *"
+      },
+      correctAns: "a"
+    },
+    {
+      question: "What is the purpose of media queries?",
+      answers: {
+        a: "responsive web design",
+        b: "can be view across many devices",
+        c: "part of advance CSS",
+        d: "all of the above"
+      },
+      correctAns: "d"
+    },
+    {
+      question: "What is hoisting in term of a function?",
+      answers: {
+        a: "rasing the function using a pulley",
+        b: "local variables that can use by the function",
+        c: "moving the declartion to the top of the code",
+        d: "calling the function"
+      },
+      correctAns: "c"
+    },
+    {
+      question: "How can you be successful in this Boot?",
+      answers: {
+        a: "responsive web design",
+        b: "can be view across many devices",
+        c: "part of advance CSS",
+        d: "all of the above"
+      },
+      correctAns: "d"
+    },
+  ]
