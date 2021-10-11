4a115f4d3a60b7dd103beaf4efd8c7105d514094
kcheykim <kosalcheykim@gmail.com>
2021-10-10 20:14:35 -0700

    keeping score and checking to make sure a button is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var buttonEl = document.querySelector(".startBtn");
 var quizEl = document.querySelector(".info-box");
-var timerEl = document.getElementById('timer');
+
+var scoreEl = document.getElementById('high-score');
 var objIndex = 0;
 var highscore = 0;
 var userScore = 0;
@@ -27,14 +28,14 @@ const quizQuestions = [
   },
   {
     question: "What makes the webpages interactive?",
-        a: "CSS",
+    a: "CSS",
     b: "javaScript",
     c: "HTML",
     d: "git",
     answer: "b"
   },
   {
-    question: "What is the command before you commit a page?",
+    question: "What is the command before you do a commit command?",
     a: "git push origin main",
     b: "git add -A",
     c: "git checkout -b main",
@@ -78,7 +79,7 @@ const quizQuestions = [
     a: "rasing the function using a pulley",
     b: "local variables that can use by the function",
     c: "moving the declartion to the top of the code",
-    d: "calling the function",
+    d: "declaring the function",
     answer: "c"
   },
   {
@@ -91,7 +92,6 @@ const quizQuestions = [
   }
 ];
 
-
 buttonEl.addEventListener("click", outputQuiz);
 
 function outputQuiz() {
@@ -103,7 +103,10 @@ function outputQuiz() {
 }
 
 function countdown() {
+  var timerEl = document.getElementById('timer');
   var timeInterval = setInterval(function() {
+    timerEl.style.width = "15%";
+    timerEl.style.float = "flex-end";
     timerEl.textContent = "Time: " + timeLeft;
 
     timeLeft--;
@@ -113,6 +116,16 @@ function countdown() {
   }, 1000);
 }
 
+function scoreKeeper() {
+  scoreEl.style.width = "85%";
+  scoreEl.style.justifyContent = "flex-start";
+
+  userScore += 5;
+  if(userScore > highscore) {
+    highscore = userScore;
+  }
+  scoreEl.textContent = "Your Score: " + userScore;
+}
 
 function makeQuiz() {
   //declare local variables for questions, 4 choices, and correct answers
@@ -139,35 +152,50 @@ function makeQuiz() {
     insertEl.innerHTML += "<label> <input type='radio' name='choices' value='b'> " + pickB + "</label> <br>";
     insertEl.innerHTML += "<label> <input type='radio' name='choices' value='c'> " + pickC + "</label> <br>";
     insertEl.innerHTML += "<label> <input type='radio' name='choices' value='d'> " + pickD + "</label> <br>";
-    insertEl.innerHTML += "<button onclick='check()'>Submit Answer</button>";
+    insertEl.innerHTML += "<button  type='submit' onclick='check()'>Submit Answer</button>";
     
     quizEl.appendChild(insertEl);
   } else {
-    console.log("done");
+    quizEl.innerHTML = "";
+    insertEl.innerHTML += "<h3>All Done</h3>";
+    insertEl.innerHTML += "<h4>Your Final Score is: " + userScore + "</h4>";
+    insertEl.innerHTML += "<label> <input type='text' name='initial'></label>"
+    quizEl.appendChild(insertEl);
   }
 }
 
 function check() {
   //checkEl gets the four choices of the questions
   var checkEl = document.getElementsByName('choices');
-  debugger;
-  console.log("In function check");
+  
+  var confirmation = document.querySelector('#form-group');
+
 
   //a local checkItem to hold the item that was selected in the radio button
-  var checkItem;
+  var checkItem = null;
   for(var i = 0; i < checkEl.length; i++) {
     //if the choice was selected, then set the checkItem to its value
     if(checkEl[i].checked) {
       checkItem = checkEl[i].value;
-      console.log("checkItem: ", checkItem);
+    }
+  }
+  if(checkItem === null) {
+    check();
+  }
+ if(checkItem == quizQuestions[objIndex].answer) {
+    confirmation.textContent = "😀 CORRECT 😀";
+    if (objIndex === quizQuestions.length){
+      confirmation.textContent = "";
+    }
+    scoreKeeper();
+  }else{
+    timeLeft = timeLeft - 10;
+    confirmation.textContent = "😭 WRONG 😭"
+    if (objIndex === quizQuestions.length){
+      confirmation.textContent = "";
     }
   }
 
-  if(checkItem == quizQuestions[objIndex].answer) {
-    userScore++;
-  }else{
-    timeLeft = timeLeft - 5;
-  }
   objIndex++;
   makeQuiz();
 }
